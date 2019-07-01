41258344d5ad989727c2c2034e5cced3d6c14f53
tombalu23 <tombalu23@gmail.com>
2019-07-01 20:02:01 +0530

    marked present using id

--- app/src/main/java/com/blah/project/myapplication123/MainActivity.java
+++ app/src/main/java/com/blah/project/myapplication123/MainActivity.java
@@ -2,8 +2,10 @@ package com.blah.project.myapplication123;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -15,17 +17,27 @@ import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity
 {
 
     //public Context context;
     Collection<CalendarDay> dat = new ArrayList<>();
-
+    String date;
+    ArrayList<JSONObject> jarray = new ArrayList<>();
+    JSONArray jsarray=new JSONArray();
+    ArrayList<String> dates= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +73,100 @@ public class MainActivity extends AppCompatActivity
 
         dat.add(ob);
         view.addDecorators(new decorator1(R.color.colorAccent,dat,this));
+
+        String name = "girish23";
+
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://attendance24.herokuapp.com/getn/"+name, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
+                Log.d("date", String.valueOf(statusCode));
+                try {
+                    JSONArray jsarray=new JSONArray(new String (response));
+
+                    for(int i=0; i<jsarray.length();i++)
+                    {
+                        date=jsarray.getJSONObject(i).getString("published");
+                        dates.add(date);
+                        String [] dateParts= dates.get(i).split("-");
+                        int year =Integer.parseInt(dateParts[0]);
+                        int month=Integer.parseInt(dateParts[1]);
+                        int day=Integer.parseInt(dateParts[2]);
+                        CalendarDay D = CalendarDay.from(year, month,day);
+                        dat.add(D);
+                        MaterialCalendarView view = findViewById(R.id.cal);
+                        view.addDecorators(new decorator1(R.color.colorAccent,dat,MainActivity.this));
+
+
+
+                    }
+
+
+
+
+
+                    JSONObject js= new JSONObject(new String(response));
+                    date=js.getString("published");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Log.d("date", date);
+
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("date", "failure");
+                Log.d("date", String.valueOf(e));
+
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+            }
+        });
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
